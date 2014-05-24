37ba6c6ce473ae62adcb462f274fec1b125fdd2b
Alex Baratti <alex.techtalk4geeks@gmail.com>
2014-05-24 16:08:03 -0700

    Fully added Alex's code into here - Copied MainActivity - Added Azimuth

--- src/ioio/examples/hello/MainActivity.java
+++ src/ioio/examples/hello/MainActivity.java
@@ -1,36 +1,35 @@
 package ioio.examples.hello;
 
-/**************************************************************************
- * Happy version 140517A...ultrasonics and motors working
- **************************************************************************/
 import ioio.lib.api.DigitalOutput;
-import ioio.lib.api.PwmOutput;
-import ioio.lib.api.Sequencer;
-import ioio.lib.api.Sequencer.ChannelConfigBinary;
-import ioio.lib.api.Sequencer.ChannelCueBinary;
-import ioio.lib.api.Sequencer.ChannelCueFmSpeed;
-import ioio.lib.api.Sequencer.ChannelCueSteps;
+import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.android.IOIOActivity;
-import android.graphics.DashPathEffect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.ScrollView;
+import android.widget.Scroller;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 /**
- * This is the main activity of the HelloIOIO example application. It displays a toggle button on the screen, which enables control of the on-board LED and controls the VicsWagon. Modified by Vic rev 140430A
+ * This is the main activity of the HelloIOIO example application. It displays a
+ * toggle button on the screen, which enables control of the on-board LED.
+ * Modified by Vic rev 130402A
  */
-public class MainActivity extends IOIOActivity
+public class MainActivity extends IOIOActivity implements SensorEventListener,
+		RylexAPI
 {
 	private ToggleButton button_;
-	public UltraSonicSensor sonar;
+	private int targetDirection = 90;
+	private int difference;
+	private UltraSonicSensor sonar;
 	private TextView mText;
 	private ScrollView mScroller;
 	/**
@@ -50,7 +49,8 @@ public class MainActivity extends IOIOActivity
 	private double roll;
 
 	/**
-	 * Called when the activity is first created. Here we normally initialize our GUI.
+	 * Called when the activity is first created. Here we normally initialize
+	 * our GUI.
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -58,20 +58,53 @@ public class MainActivity extends IOIOActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		button_ = (ToggleButton) findViewById(R.id.button);
-		mText = (TextView) findViewById(R.id.logText);
 		mScroller = (ScrollView) findViewById(R.id.scroller);
+		mText = (TextView) findViewById(R.id.logText);
+
+		// Compass stuff
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		sensorAccelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorMagneticField = sensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+		valuesAccelerometer = new float[3];
+		valuesMagneticField = new float[3];
+
+		matrixR = new float[9];
+		matrixI = new float[9];
+		matrixValues = new float[3];
+	}
+
+	@Override
+	public void onPause()
+	{
+		sensorManager.unregisterListener(this, sensorAccelerometer);
+		sensorManager.unregisterListener(this, sensorMagneticField);
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume()
+	{
+
+		sensorManager.registerListener(this, sensorAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, sensorMagneticField,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		super.onResume();
 	}
 
 	/**
-	 * This is the thread on which all the IOIO activity happens. It will be run every time the application is resumed and aborted when it is paused. The method setup() will be called right after a connection with the IOIO has been established (which might happen several times!). Then, loop() will be called repetitively until the IOIO gets disconnected
+	 * This is the thread on which all the IOIO activity happens. It will be run
+	 * every time the application is resumed and aborted when it is paused. The
+	 * method setup() will be called right after a connection with the IOIO has
+	 * been established (which might happen several times!). Then, loop() will
+	 * be called repetitively until the IOIO gets disconnected
 	 */
 	class Looper extends BaseIOIOLooper
 	{
 		private DigitalOutput led_;// The on-board LED
-		private int pulseWidth = 10;// microseconds
-		private int rightStepperMotorPeriod = 60000;
-		private DigitalOutput rightMotorClockPulse;
-		private DigitalOutput leftMotorClockPulse;
 		private DigitalOutput motorEnable; // Both motors
 		private DigitalOutput rightMotorClock; // Step right motor
 		private DigitalOutput leftMotorClock; // Step left motor
@@ -79,52 +112,42 @@ public class MainActivity extends IOIOActivity
 		private DigitalOutput rightMotorControl; // Motor decay mode
 		private DigitalOutput rightMotorDirection;
 		private DigitalOutput leftMotorDirection;
-		private DigitalOutput motorControllerControl;// Decay mode selector, high = slow decay, low = fast decay
-		private static final int MOTOR_ENABLE_PIN = 3;// Low turns off all power to both motors
-		private static final int MOTOR_RIGHT_DIRECTION_OUTPUT_PIN = 20;// High = clockwise, low = ccw
-		private static final int MOTOR_LEFT_DIRECTION_OUTPUT_PIN = 21;
-		private static final int MOTOR_CONTROLLER_CONTROL_PIN = 6;// For both
-		private static final int REAR_STROBE_ULTRASONIC_OUTPUT_PIN = 14;// output from ioio board
-		private static final int MOTOR_HALF_FULL_STEP_PIN = 7;// For both motors
-		private static final int MOTOR_RESET = 22;// For both motors
-		private static final int MOTOR_CLOCK_LEFT_PIN = 27;
-		private static final int MOTOR_CLOCK_RIGHT_PIN = 28;
-		private Sequencer sequencer_;
-		private Sequencer.ChannelCueBinary stepperDirCue_ = new ChannelCueBinary();
-		private Sequencer.ChannelCueSteps stepperStepCue_ = new ChannelCueSteps();
-		private Sequencer.ChannelCueFmSpeed stepperFMspeedCue_ = new ChannelCueFmSpeed();
-		private Sequencer.ChannelCue[] cue_ = new Sequencer.ChannelCue[]
-		{ stepperFMspeedCue_ };
-		final ChannelConfigBinary stepperDirConfig = new Sequencer.ChannelConfigBinary(false, false, new DigitalOutput.Spec(MOTOR_RIGHT_DIRECTION_OUTPUT_PIN));
-		private int rightMotorSpeed;
-		private DigitalOutput halfFull;
-		private DigitalOutput reset; // Must be true for motors to run.
-		private DigitalOutput control;// Decay mode selector high = slow, low = fast mode
-
-		// final ChannelConfigSteps stepperStepConfig = new
-		// ChannelConfigSteps(new DigitalOutput.Spec(MOTOR_CLOCK_RIGHT_PIN));
-		// final ChannelConfigFmSpeed stepperFMspeedConfig = new ChannelConfigFmSpeed(Clock.CLK_2M, 10, new
-		// DigitalOutput.Spec(MOTOR_CLOCK_RIGHT_PIN));
-		// final ChannelConfig[] config = new ChannelConfig[] {stepperFMspeedConfig};
+		
 		/**
-		 * Called every time a connection with IOIO has been established. Typically used to open pins.
+		 * Called every time a connection with IOIO has been established.
+		 * Typically used to open pins.
 		 * 
-		 * @throws ConnectionLostExceptio
-		 *             when IOIO connection is lost. when IOIO connection lost.
+		 * @throws ConnectionLostException
+		 *             when IOIO connection is lost. when IOIO connection is
+		 *             lost.
 		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
 		 */
+		
 		@Override
-		protected void setup() throws ConnectionLostException
+		protected void setup() throws ConnectionLostException 
 		{
 			sonar = new UltraSonicSensor(ioio_);
 			led_ = ioio_.openDigitalOutput(0, true);
-			rightMotorDirection = ioio_.openDigitalOutput(20, true);// vicswagon goes forward
-			leftMotorDirection = ioio_.openDigitalOutput(21, false);
+			rightMotorDirection = ioio_.openDigitalOutput(20, false);
+			leftMotorDirection = ioio_.openDigitalOutput(21, true);
 			motorCongtrollerReset = ioio_.openDigitalOutput(22, true);
-			motorEnable = ioio_.openDigitalOutput(3, true);// Must be true for motors to run
-			rightMotorClock = ioio_.openDigitalOutput(28, false);// Each pulse moves motor one step
-			leftMotorClock = ioio_.openDigitalOutput(27, false);
-			rightMotorControl = ioio_.openDigitalOutput(6, false);// Both motors, low => fast motor decay mode
+			motorEnable = ioio_.openDigitalOutput(3, true);// Must be true for
+															// motors to run
+			rightMotorClock = ioio_.openDigitalOutput(28, false);// Each pulse
+																	// moves
+																	// motor one
+																	// step
+			leftMotorClock = ioio_.openDigitalOutput(27, false);// Each pulse
+																// moves motor
+																// one step
+			rightMotorControl = ioio_.openDigitalOutput(6, false);// Both
+																	// motors,
+																	// low =>
+																	// fast
+																	// motor
+																	// decay
+																	// mode
+			log("Hello world!");
 		}
 
 		/**
@@ -139,19 +162,41 @@ public class MainActivity extends IOIOActivity
 		{
 			if (button_.isChecked())
 			{
-				led_.write(false);
+				led_.write(false); //turns light on
 				try
 				{
-					Thread.sleep(1000);
-					// led_.write(true);
+					SystemClock.sleep(1000);
+					sonar.read();
+					log(String.valueOf(sonar.getFrontDistance()));
+					hugRightDistance(20);
+					Thread.sleep(10);
+					led_.write(true);
 					// rightMotorClock.write(true);
 					// rightMotorClock.write(false);
 					// leftMotorClock.write(true);
 					// leftMotorClock.write(false);
-					sonar.read();
-					log(String.valueOf(sonar.getFrontDistance() + " " + sonar.getLeftDistance()));
-				} catch (InterruptedException e)
+					difference = (int) (targetDirection - azimuth);
+					if (difference < -180)
+					{
+						difference += 360;
+					} else if (difference > 180)
+					{
+						difference -= 360;
+					}
+					if (difference > 3)
+					{
+						turnLeft();
+					} else if (difference < 3)
+					{
+						turnRight();
+					} else
+					{
+						goForward();
+						log("STRAIGHT");
+					}
+				} catch (Exception e)
 				{
+					log(e.getClass().getName() + ", " + e.getMessage());
 				}
 			} else
 			{
@@ -159,40 +204,44 @@ public class MainActivity extends IOIOActivity
 			}
 		}
 
-		public void onSensorChanged(SensorEvent event)
+		public void goForward() throws Exception
 		{
-			switch (event.sensor.getType())
-			{
-			case Sensor.TYPE_ACCELEROMETER:
-				for (int i = 0; i < 3; i++)
-				{
-					valuesAccelerometer[i] = event.values[i];
-				}
-				break;
-			case Sensor.TYPE_MAGNETIC_FIELD:
-				for (int i = 0; i < 3; i++)
-				{
-					valuesMagneticField[i] = event.values[i];
-				}
-				break;
-			}
-			boolean success = SensorManager.getRotationMatrix(matrixR, matrixI, valuesAccelerometer, valuesMagneticField);
-			if (success)
-			{
-				SensorManager.getOrientation(matrixR, matrixValues);
-				synchronized (this)
-				{
-					azimuth = Math.toDegrees(matrixValues[0]);
-					pitch = Math.toDegrees(matrixValues[1]);
-					roll = Math.toDegrees(matrixValues[2]);
-					if (Math.abs(lastAzimuth - (int) azimuth) > 2)
-					{
-						log("hi azimuth");
-						log("azimuth = " + (int) (azimuth));
-						lastAzimuth = (int) (azimuth);
-					}
-				}
-			}
+			rightMotorClock.write(true);
+			rightMotorClock.write(false);
+			leftMotorClock.write(true);
+			leftMotorClock.write(false);
+		}
+		
+		public void hugRightDistance(int distance) throws Exception
+		{
+			log("Going to read the sensors");
+			sonar.read();
+			log("Read the sensors");
+			log(String.valueOf(sonar.getRightDistance()));
+//			if (sonar.getRightDistance() < distance)
+//			{
+//				turnTo(-3);
+//			} else if (sonar.getRightDistance() > distance)
+//			{
+//				turnTo(3);
+//			} else
+//			{
+//				goForward();
+//			}
+		}
+
+		public void turnRight() throws Exception
+		{
+			rightMotorClock.write(false);
+			leftMotorClock.write(true);
+			leftMotorClock.write(false);
+		}
+
+		public void turnLeft() throws Exception
+		{
+			rightMotorClock.write(true);
+			rightMotorClock.write(false);
+			leftMotorClock.write(false);
 		}
 	}
 
@@ -201,22 +250,11 @@ public class MainActivity extends IOIOActivity
 	 * 
 	 * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
 	 */
-	@Override
-	protected IOIOLooper createIOIOLooper()
-	{
-		return new Looper();
-	}
-
-	/**
-	 * Writes a message to the Dashboard instance.
-	 * 
-	 * @param msg
-	 *            the message to write
-	 */
 	public void log(final String msg)
 	{
 		runOnUiThread(new Runnable()
 		{
+
 			public void run()
 			{
 				mText.append(msg);
@@ -225,4 +263,67 @@ public class MainActivity extends IOIOActivity
 			}
 		});
 	}
+
+	public void onSensorChanged(SensorEvent event)
+	{
+		switch (event.sensor.getType())
+		{
+		case Sensor.TYPE_ACCELEROMETER:
+			for (int i = 0; i < 3; i++)
+			{
+				valuesAccelerometer[i] = event.values[i];
+			}
+			break;
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			for (int i = 0; i < 3; i++)
+			{
+				valuesMagneticField[i] = event.values[i];
+			}
+			break;
+		}
+
+		boolean success = SensorManager.getRotationMatrix(matrixR, matrixI,
+				valuesAccelerometer, valuesMagneticField);
+
+		if (success)
+		{
+			SensorManager.getOrientation(matrixR, matrixValues);
+			synchronized (this)
+			{
+				azimuth = Math.toDegrees(matrixValues[0]);
+				pitch = Math.toDegrees(matrixValues[1]);
+				roll = Math.toDegrees(matrixValues[2]);
+				if (Math.abs(lastAzimuth - (int) azimuth) > 2)
+				{
+					log("azimuth = " + (int) (azimuth));
+					lastAzimuth = (int) (azimuth);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	protected IOIOLooper createIOIOLooper()
+	{
+		return new Looper();
+	}
+
+	public void turnTo(int degrees)
+	{
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy)
+	{
+
+	}
+
+	public void forfeit()
+	{
+		log("NullPointerException\nThank you for using the forfeit method!");
+		throw new NullPointerException();
+	}
+
 }
---
+++ src/ioio/examples/hello/RylexAPI.java
@@ -0,0 +1,7 @@
+package ioio.examples.hello;
+
+public interface RylexAPI
+{
+	void turnTo(int degrees);
+
+}
